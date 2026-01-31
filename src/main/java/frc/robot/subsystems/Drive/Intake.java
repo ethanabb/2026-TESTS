b66d2c8584b9dd04c729ba73037ca7b4b24418d0
Daenaq <bobthedabber7@gmail.com>
2026-01-30 15:50:26 -1000

    intake motors

--- src/main/java/frc/robot/subsystems/Drive/Intake.java
+++ src/main/java/frc/robot/subsystems/Drive/Intake.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import com.revrobotics.spark.SparkFlex; // For the SPARK Flex controller
 import com.revrobotics.spark.SparkMax;  // For the SPARK MAX controller (if using a solo adapter)
 import com.revrobotics.spark.SparkLowLevel.MotorType; // To specify brushless motor type
+import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.SparkClosedLoopController; // For advanced control like PID loops (optional)
 import com.revrobotics.spark.SparkRelativeEncoder; // For accessing the built-in encoder (optional)
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -35,36 +36,83 @@ public class Intake extends SubsystemBase{
     private final DigitalInput pivotLimitSwitch = new DigitalInput(5);
 
     private double setSpeed = 0.5;
- 
-
+    
+    // gets encoder angle in degress and sets soft limits
+    private double pivotCurAngle = pivotEncoder.get() * 360;
+    private final double pivotMinAngle = 0.0;
+    private final double pivotMaxAngle = 90.0;
 
     public Intake(){
     // prevents intake jerk
     TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
     intakeConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.25;
 
+    // SparkFlexConfig pivotConfig = new SparkFlexConfig();
+
     leaderIntake.getConfigurator().apply(intakeConfig);
     followerIntake.getConfigurator().apply(intakeConfig);
-    }
 
+    }
+    
     public Command runIntake(){
         return new RunCommand(() -> {
             leaderIntake.set(setSpeed);
             followerIntake.set(-setSpeed);
-        }, 
-        this );
+        }
+        // , this 
+        );
     }
 
-    public Command raiseIntake(){
-        return new RunCommand(() -> {
+    // public boolean safeZone(){
+    //     double pivotCurAngle = pivotEncoder.get() * 360;
+    //     if (pivotCurAngle >= pivotMinAngle && pivotCurAngle <= pivotMaxAngle){
+    //         return true; 
+    //     } else { 
+    //         return false; 
+    //     }
+    // }
 
-        });
+    public boolean canRaise(){
+        double pivotCurAngle = pivotEncoder.get() * 360;
+        if (pivotCurAngle < pivotMaxAngle){
+            return true; 
+        } else { 
+            return false; 
+        }
+    }
+
+    public boolean canLower(){
+        double pivotCurAngle = pivotEncoder.get() * 360;
+        if (pivotCurAngle > pivotMinAngle){
+            return true; 
+        } else { 
+            return false; 
+        }
     }
 
     public Command lowerIntake(){
         return new RunCommand(() -> {
-
-        });
+            if (!pivotLimitSwitch.get() && canLower()){
+                pivotArm.set(0.01);  // CONFIRM ROTATION DIRECTION BEFORE RUNNING THIS CODE
+            } else {
+                pivotArm.set(0);
+            }
+        }
+        // ensures when this command runs, it has sole control of the intake subsystem
+        , this
+        );
+    }
+    
+    public Command raiseIntake(){
+        return new RunCommand(() -> {
+            if (canRaise()){
+                pivotArm.set(-0.01); // CONFIRM ROTATION DIRECTION BEFORE RUNNING THIS CODE
+            } else {
+                pivotArm.set(0);
+            }
+        }
+        , this
+        );
     }
 
     public Command stopAll(){
